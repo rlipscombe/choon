7c00db213b2615549fe21b7accbffd005a161be0
Roger Lipscombe <roger@differentpla.net>
2021-04-02 16:46:22 +0100

    Remove gradle-generated nonsense
    The 'gradle init' command generates some placeholder code, which we
    don't need.
    
    Delete it.

--- app/src/main/kotlin/choon/app/App.kt
+++ app/src/main/kotlin/choon/app/App.kt
@@ -3,12 +3,6 @@
  */
 package choon.app
 
-import choon.utilities.StringUtils
-
-import org.apache.commons.text.WordUtils
-
 fun main() {
-    val tokens = StringUtils.split(MessageUtils.getMessage())
-    val result = StringUtils.join(tokens)
-    println(WordUtils.capitalize(result))
+    println("Hello World")
 }
